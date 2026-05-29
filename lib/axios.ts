e8e38b7ce18e6a0357d8d11df13972c0c77c13e5
pius-code <obliepius13@gmail.com>
2026-05-29 08:22:55 +0000

    feat: enhance axios configuration with response interceptor for 401 errors

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,17 +1,29 @@
-import axios from "axios"
-import { getSession } from "next-auth/react"
+import axios from "axios";
+import { getSession, signOut } from "next-auth/react";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080",
   headers: { "Content-Type": "application/json" },
-})
+});
 
 api.interceptors.request.use(async (config) => {
-  const session = await getSession()
+  const session = await getSession();
   if (session?.accessToken) {
-    config.headers.Authorization = `Bearer ${session.accessToken}`
+    config.headers.Authorization = `Bearer ${session.accessToken}`;
   }
-  return config
-})
+  return config;
+});
 
-export default api
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      if (typeof window !== "undefined") {
+        signOut({ callbackUrl: "/login" });
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
